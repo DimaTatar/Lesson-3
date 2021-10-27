bb9812100d0925d6fd8a9673061b1fe1f9a50453
Dima <drdima68@yandex.ua>
2021-10-27 19:03:40 +0300

    Finish!!!!!

--- Lesson 3/ViewController.swift
+++ Lesson 3/ViewController.swift
@@ -7,25 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var passfordName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
     }
-
-    @IBAction func rightName() {
-        let allertController = UIAlertController(title: "Ooops!", message: "Your name is User😉", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok", style: .default)
-        allertController.addAction(action)
-        self.present(allertController,animated: true,completion: nil)
-    }
-    @IBAction func rightPassford() {
-        let allertController = UIAlertController(title: "Ooops!", message: "Your passford is Passford😉", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok", style: .default)
-        allertController.addAction(action)
-        self.present(allertController,animated: true,completion: nil)
+    @IBAction func actionButton(_ sender: UIButton) {
+        if userName.text == "User" && passfordName.text == "Passford"{
+            userName.text = "User"
+            passfordName.text = "Passford"
+           
+        } else if  passfordName.text != "Passford" {
+        self.alert(title: "Invalid login or passford", message: "Please, enter correct login and passford", style: .alert)
+            passfordName.text?.removeAll()
+            } else {
+            self.alert(title: "Invalid login or passford", message: "Please, enter correct login and passford", style: .alert)
+        }
     }
     
-}
+    @IBAction func rightName() {
+        self.alert(title: "Ooops!", message: "Your name is User😉", style: .alert)
+        
+    }
+    @IBAction func rightPassford() {
+        self.alert(title: "Ooops!", message: "Your passford is Passford😉", style: .alert)
+    
+    }
+    
+    @IBAction  func unwind(for segue: UIStoryboardSegue) {
+        userName.text?.removeAll()
+        passfordName.text?.removeAll()
+    }
+   
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+    func alert(title: String, message: String, style: UIAlertController.Style) {
+        let allertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "Ok", style: .default)
+        allertController.addAction(action)
+        self.present(allertController,animated: true,completion: nil)
+    }
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeViewController = segue.destination as? WelcomeViewController else {return}
+        welcomeViewController.nameLabel += userName.text ?? "Error"
+    }
+    
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case userName:
+            passfordName.becomeFirstResponder()
+        default:
+            passfordName.resignFirstResponder()
+            
+        }
+        return true
+    }
+       
+   
+
+
+}
--- Lesson 3/WelcomeViewController.swift
+++ Lesson 3/WelcomeViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 class WelcomeViewController: UIViewController {
     @IBOutlet weak var hiNameLabel: UILabel!
     
+    var nameLabel: String! = "Welcome, "
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        hiNameLabel.text = nameLabel
 
  
     }
